a50fb3bf13e0f6d5679f14edf6e71ef40419b273
Filipe da Cruz Ribeiro <filipe.cruz.ribeiro@gmail.com>
2021-08-26 11:18:32 -0300

    Update CDU.py

--- CDU.py
+++ CDU.py
@@ -1,11 +1,3 @@
-# u = input()
-# u = int(u)
-# hundred = int(u / 100)
-# decimal = int((u % 100) / 10)
-# unit = int(u % 10)
-# total = (unit.__str__() + decimal.__str__() + hundred.__str__())
-# print(int(total))
-
 u = input()
 u = int(u)
 hundred = int(u / 100)
